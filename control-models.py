98b6e50df0d109b14f448132c4a992f95f02e9ac
Noah Elazar <noahbelazar@gmail.com>
2024-12-12 11:38:42 -0500

    Finished grid search code

--- control-models.py
+++ control-models.py
@@ -11,14 +11,36 @@ import neuralnet
 
 
 # Read locations of datasets
-dataset_locations = []
-with open(Path("./data/datasets.csv"), 'r') as dataset_locations_file:
-    dataset_locations_reader = csv.DictReader(dataset_locations_file)
-    for dataset in dataset_locations_reader:
-        dataset_locations.append(dataset)
+def get_datasets():
+    dataset_locations = []
+    with open(Path("./data/datasets.csv"), 'r') as dataset_locations_file:
+        dataset_locations_reader = csv.DictReader(dataset_locations_file)
+        for dataset in dataset_locations_reader:
+            dataset_locations.append(dataset)
+
+    return dataset_locations
+
+# Changes the dataset label column to "label"
+def relabel(dataset_path, label_col):
+    with open(dataset_path, 'r') as dataset:
+        lines = dataset.readlines()
+
+    column_headers = lines[0].split(',')
+    column_headers = list(map(lambda x: x.replace(label_col, "label"), column_headers))
+    lines[0] = ','.join(column_headers)
+    
+    with open(dataset_path, 'w') as dataset:
+        dataset.writelines(lines)
+
+# Preprocess datasets
+def preprocess():
+    dataset_locations = get_datasets()
+    for dataset_info in dataset_locations:
+        relabel(dataset_info['path'], dataset_info['label-col'])
 
 # Grid search for each dataset
 def gridsearch():
+    dataset_locations = get_datasets()
     for dataset_info in dataset_locations:
         print(f"Grid search for {dataset_info['name']}")
         neuralnet.gridsearch_mode(dataset_info['path'], dataset_info['name'], 0.75, 12345, True)
